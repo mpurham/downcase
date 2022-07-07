eee5eceb6054df343ac7c8a26e7af42fc9d29a4e
Marcell Purham <mpurham@cybercell.local>
2022-07-07 00:11:34 -0500

    Adding downcase script

---
+++ Downcase.js
@@ -0,0 +1,10 @@
+/*
+    name: Downcase
+    description: Converts your text to lowercase.
+    author: Marcell Purham
+    tags: downcase,lowercase,tolower
+*/
+
+function main(input) {
+    input.text = input.text.toLowerCase();
+}
